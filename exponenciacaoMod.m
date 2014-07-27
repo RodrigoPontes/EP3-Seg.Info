62dc9a62498a0c562d1f0fdf25a3e8da47312bfb
RodrigoPontes <rodrgarcia@gmail.com>
2014-07-26 21:16:50 -0300

    1.1 Exponenciação funcionando

--- exponenciacaoMod.m
+++ exponenciacaoMod.m
@@ -1,18 +1,17 @@
 function [ resp ] = exponenciacaoMod( a, b, n )
 
-% c = 0;
-% f = 1;
-% k = floor(log(b));
-bi = bitget(b,floor(log2(b))+1:1);
-% for i=k:-1:0
-%     c = 2*c;
-%     f = mod((f*f),n);
-%     if bi 
-%         c = c + 1;
-%         f = mod( (f * a) , n);
-%     end
-% end
-% ALGORITMO NAO FUNCIONAL AINDA. POR ENQUANTO, FAZENDO O MAIS SIMPLES
-resp = mod((a^b),n);
+c = 0;
+f = 1;
+k = floor(log2(b));
+for i = k:-1:0
+    c = 2*c;
+    f = mod(f*f,n);
+    if bitget(b,i+1)
+        c = c+1;
+        f = mod(f*a,n);
+    end
+end
+
+resp = f;
 
 end
